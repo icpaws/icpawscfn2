0d38ad1af4a18c5ab02dc05dca7fb827f983018d
icpaws <52262834+icpaws@users.noreply.github.com>
2019-06-27 18:31:55 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "main-public-1" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "eu-west-1a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "main-public-1"
@@ -31,7 +31,7 @@ resource "aws_subnet" "main-private-1" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.21.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone = "eu-west-1a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "main-private-1"
@@ -42,7 +42,7 @@ resource "aws_subnet" "main-private-2" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.22.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone = "eu-west-1b"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "main-private-2"
